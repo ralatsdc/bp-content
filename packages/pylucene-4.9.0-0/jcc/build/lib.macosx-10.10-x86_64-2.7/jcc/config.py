7d2458481a267fe39a88228bfee57262c85719c8
Raymond LeClair <raymond.leclair@gmail.com>
2015-06-02 10:21:14 -0700

    Added pylucene-4.9.0-0.

---
+++ packages/pylucene-4.9.0-0/jcc/build/lib.macosx-10.10-x86_64-2.7/jcc/config.py
@@ -0,0 +1,8 @@
+
+INCLUDES=['/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home/include', '/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home/include/darwin']
+CFLAGS=['-fno-strict-aliasing', '-Wno-write-strings']
+DEBUG_CFLAGS=['-O0', '-g', '-DDEBUG']
+LFLAGS=['-L/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home/jre/lib', '-ljava', '-L/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home/jre/lib/server', '-ljvm', '-Wl,-rpath', '-Wl,/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home/jre/lib', '-Wl,-rpath', '-Wl,/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home/jre/lib/server']
+IMPLIB_LFLAGS=[]
+SHARED=True
+VERSION="2.20"
